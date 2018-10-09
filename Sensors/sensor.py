48f71297615fd2d12ae29152f387bcc8f5123dcc
DANIEL GRUMBERG <dany.grumberg@gmail.com>
2018-10-09 16:31:31 +0100

    Implement DepthSensor class
    It exposes a class level method init_gpio_pins to initialise GPIO functionality.
    Furthermore, it relies on a AddressBus type that exposes a is_valid_index method
    to check the sensor can be addressed. The module can be used as before to to
    power the simple sensor test.

--- Sensors/sensor.py
+++ Sensors/sensor.py
@@ -2,51 +2,68 @@
 #Libraries
 import RPi.GPIO as GPIO
 import time
- 
-GPIO.setmode(GPIO.BCM)
- 
-#set GPIO Pins
-GPIO_TRIGGER = 23
-GPIO_ECHO = 24
- 
-#set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-def distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
- 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
- 
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
- 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
- 
-    return distance
+
+class DepthSensor:
+    GPIO_TRIGGER = 0
+    GPIO_ECHO = 0
+    address_bus = []
+
+    @classmethod
+    def init_gpio_pins(cls):
+        GPIO.setmode(GPIO.BCM)
+        #set GPIO direction (IN / OUT)
+        GPIO.setup(cls.GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(cls.GPIO_ECHO, GPIO.IN)
+
+    def __init__(self, index):
+        assert(address_bus.is_valid_index(index))
+        self.index = index
+
+    def distance(self):
+        # set Trigger to HIGH
+        GPIO.output(GPIO_TRIGGER, True)
+
+        # set Trigger after 0.01ms to LOW
+        time.sleep(0.00001)
+        GPIO.output(GPIO_TRIGGER, False)
+
+        start_time = time.time()
+        stop_time = time.time()
+
+        # save start_time
+        while GPIO.input(GPIO_ECHO) == 0:
+            start_time = time.time()
+
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO) == 1:
+            stop_time = time.time()
+
+        # time difference between start and arrival
+        TimeElapsed = stop_time - start_time
+        # multiply with the sonic speed (34300 cm/s)
+        # and divide by 2, because there and back
+        distance = (TimeElapsed * 34300) / 2
+
+        return distance
 
 if __name__ == '__main__':
+    class DummyAddressBus:
+        def is_valid_index(idx):
+            return True
+
     try:
+        DepthSensor.GPIO_TRIGGER = 23
+        DepthSensor.GPIO_ECHO = 23
+        DepthSensor.address_bus = DummyAddressBus()
+
+        DepthSensor.init_gpio_pins()
+
+        sensor = DepthSensor(0)
         while True:
-            dist = distance()
+            dist = sensor.distance()
             print ("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
- 
+
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Measurement stopped by User")
