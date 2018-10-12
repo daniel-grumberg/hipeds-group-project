9b99a4a95dd6162570b354b56848d7185bcf0005
roymiles <roy.miles12@gmail.com>
2018-10-12 13:22:12 +0100

    Update, retrieve old ip addresses of pi from aws

--- sensors/sensors-aws.py
+++ sensors/sensors-aws.py
@@ -11,104 +11,149 @@ sensor values on the Amazon AWS database
 import pymysql
 import time
 
+
+
+'''
+	Useful lambda function for calculating current time in milli-seconds
+'''
+current_milli_time = lambda: int(round(time.time() * 1000))
+
+
 host_name = "hipeds-group.ckjqazo6ukkn.eu-west-2.rds.amazonaws.com"
 db_username = "hipeds"
 db_password = "YHADXjMrX3nGssAw"
 db_name = "innodb" 	
+
+class aws_db:
 	
-# Open database connection
-db = pymysql.connect(host_name, db_username, db_password, db_name)
-cur = db.cursor()
-
-current_milli_time = lambda: int(round(time.time() * 1000))
-
-# Utility fuctions
-
-#ALTER TABLE DepthSensorReadings
-#ADD FOREIGN KEY (sensorId) REFERENCES DepthSensors(idDepthSensors);
-
-'''
-	Add sensor reading
-'''
-def add_reading(readings):
-	for sensorId, value in readings:
-		sql = "INSERT INTO DepthSensorReadings (sensorId, value, timestamp) VALUES (%s, %s, %s)" % (sensorId, value, current_milli_time());
-		print("EXECUTING ---> " + sql)
-		cur.execute(sql)
-
-'''
-	This function will raise an error, if unable to connect to the database
-'''
-def check_con():
-	cur.execute("SELECT VERSION()")
-	data = cur.fetchone()
-	print("Database version : %s " % data)
+	def __init__(self):
+		print("Connecting to aws database...")
+		#try:
+		self.db = pymysql.connect(host_name, db_username, db_password, db_name)
+		self.cur = self.db.cursor()
+		#except:
+		#	print("Failed to connect")
 	
-'''
-	Return the most recent sensor reading for a list of sensors
-'''
-def get_last_readings(sensorIds):
-	# The below method can return multiple readings from one sensor, as opposed to getting at least one from each
-	#sql = "SELECT * FROM DepthSensorReadings WHERE sensorId IN (%s) ORDER BY timestamp DESC" % ",".join(map(str, sensorIds));
-	#print("EXECUTING ---> " + sql)
-	#cur.execute(sql)
-	
-	data = []
-	for sensorId in sensorIds:
-		sql = "SELECT * FROM DepthSensorReadings WHERE sensorId = %s ORDER BY timestamp DESC LIMIT 1" % sensorId
-		print("EXECUTING ---> " + sql)
-		cur.execute(sql)
-		data.append(cur.fetchall())
+	'''
+		Add sensor reading
+	'''
+	def add_reading(self, readings):
+		for sensorId, value in readings:
+			sql = "INSERT INTO DepthSensorReadings (sensorId, value, timestamp) VALUES (%s, %s, %s)" % (sensorId, value, current_milli_time());
+			print("EXECUTING ---> " + sql)
+			self.cur.execute(sql)
+
+	'''
+		This function will raise an error, if unable to connect to the database
+	'''
+	def check_con(self):
+		self.cur.execute("SELECT VERSION()")
+		data = self.cur.fetchone()
+		print("Database version : %s " % data)
 		
-	print(data)
-	return data
-	
-'''
-	Deletes all the previous depth sensor readings and resets the PK
-'''
-def delete_previous_readings():
-	cur.execute("TRUNCATE TABLE DepthSensorReadings")
-	
-'''
-	Deletes all the sensors ids
-'''
-def delete_sensors():
-	cur.execute("TRUNCATE TABLE DepthSensors")
-	
-'''
-	List the Ids of all the sensors
-'''
-def get_sensor_ids():
-	cur.execute("SELECT idDepthSensors From DepthSensors")
-	result = cur.fetchall()
-	print(result)
-	return result
-	
-'''
-	Add a list of sensor ids
-'''
-def add_sensors(sensorIds):
-	for id in sensorIds: # Ideally in a single query..
-		sql = "INSERT INTO DepthSensors (idDepthSensors) VALUES (%s)" % id;
+	'''
+		Return the most recent sensor reading for a list of sensors
+	'''
+	def get_last_readings(self, sensorIds):
+		# The below method can return multiple readings from one sensor, as opposed to getting at least one from each
+		#sql = "SELECT * FROM DepthSensorReadings WHERE sensorId IN (%s) ORDER BY timestamp DESC" % ",".join(map(str, sensorIds));
+		#print("EXECUTING ---> " + sql)
+		#cur.execute(sql)
+		
+		data = []
+		for sensorId in sensorIds:
+			sql = "SELECT * FROM DepthSensorReadings WHERE sensorId = %s ORDER BY timestamp DESC LIMIT 1" % sensorId
+			print("EXECUTING ---> " + sql)
+			self.cur.execute(sql)
+			data.append(self.cur.fetchall())
+			
+		print(data)
+		return data
+		
+	'''
+		Deletes all the previous depth sensor readings and resets the PK
+	'''
+	def delete_previous_readings(self):
+		self.cur.execute("TRUNCATE TABLE DepthSensorReadings")
+		
+	'''
+		Deletes all the sensors ids
+	'''
+	def delete_sensors(self):
+		self.cur.execute("TRUNCATE TABLE DepthSensors")
+		
+	'''
+		List the Ids of all the sensors
+	'''
+	def get_sensor_ids(self):
+		self.cur.execute("SELECT idDepthSensors From DepthSensors")
+		result = self.cur.fetchall()
+		print(result)
+		return result
+		
+	'''
+		Add a list of sensor ids
+	'''
+	def add_sensors(self, sensorIds):
+		for id in sensorIds: # Ideally in a single query..
+			sql = "INSERT INTO DepthSensors (idDepthSensors) VALUES (%s)" % id;
+			print("EXECUTING ---> " + sql)
+			self.cur.execute(sql)
+			
+			
+	def update_ip(self, new_ip):
+		sql = "INSERT INTO IpAddresses (IpAddress, timestamp) VALUES ('%s', %s)" % (new_ip, current_milli_time());
 		print("EXECUTING ---> " + sql)
-		cur.execute(sql)
+		self.cur.execute(sql)
+	
+	def get_last_ip(self):
+		sql = "SELECT IpAddress FROM IpAddresses ORDER BY timestamp DESC LIMIT 1"
+		print("EXECUTING ---> " + sql)
+		self.cur.execute(sql)
+		last_ip = self.cur.fetchone()
+		
+		if last_ip is None:
+			print("No last known ip")
+		else:
+			#print("Last ip address = " + last_ip)
+			print(last_ip)
+			
+		return last_ip
+	
+			
+	def close(self):
+		self.db.close()
 
-# End utility functions
+'''
+	Unit test
+'''
+def test():
+	db = aws_db()
 
-check_con()
+	db.check_con()
 
-delete_previous_readings()
-delete_sensors()
-add_sensors([1,2,3,4]) # 4 Sensors
-get_sensor_ids()
+	'''db.delete_previous_readings()
+	db.delete_sensors()
+	db.add_sensors([1,2,3,4]) # 4 Sensors
+	db.get_sensor_ids()
 
-get_last_readings([1,2,3,4])
+	db.get_last_readings([1,2,3,4])
 
-# sensorId must be a valid Id from DepthSensors, else will fail foreign key constraint
-sensorIds = [1]
-readings = [(1, 8)]
-add_reading(readings)
-get_last_readings(sensorIds)
-delete_previous_readings()
+	# sensorId must be a valid Id from DepthSensors, else will fail foreign key constraint
+	sensorIds = [1]
+	readings = [(1, 8)]
+	db.add_reading(readings)
+	db.get_last_readings(sensorIds)
+	db.delete_previous_readings()'''
+	
+	db.get_last_ip()
+	db.update_ip("192.168.blah")
+	db.get_last_ip()
+	db.update_ip("192.168.blah2")
+	db.get_last_ip()
 
-db.close()
+	db.close()
+	
+	
+# Call unit test
+test()
