7d6f6edc57c5cf167e7f4caeac04d02bebcef934
George Bisbas <g.bisbas18@imperial.ac.uk>
2018-10-18 18:15:17 +0100

    Update summary

--- demo_merging/loop_read.m
+++ demo_merging/loop_read.m
@@ -1,13 +1,14 @@
 function [ pc ] = loop_read(model_name)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+% Input : model_name , string
+% Output: Point Cloud object
 
+fprintf(' \n *** Reading ply file...');
 model_filename = strcat(model_name, '.ply');
 [~, PTS, ~, ~] = plyread(model_filename, 'face');
 pc = pointCloud(PTS);
 figure
 pcshow(pc);
-fprintf('*** Reading ply file \n');
+fprintf('...DONE\n');
 
 end
 
